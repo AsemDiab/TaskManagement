1dd846c2303dad8ceb4f379dc6303a2ff173db96
AsemDiab <asemhesham55@gmail.com>
2026-02-18 22:37:36 +0200

    add logic

---
+++ dist/script.js
@@ -0,0 +1,66 @@
+var TaskManager = /** @class */ (function () {
+    function TaskManager() {
+        this.key = "tasks";
+        this.tasks = TasksStorageManager.load(this.key);
+    }
+    TaskManager.prototype.addTask = function (task) {
+        this.tasks.push({ task: task, status: false });
+        TasksStorageManager.store(this.key, this.tasks);
+    };
+    TaskManager.prototype.deleteTask = function (index) {
+        if (index > -1)
+            this.tasks.splice(index, 1);
+        TasksStorageManager.store(this.key, this.tasks);
+    };
+    TaskManager.prototype.toggleStatus = function (index) {
+        if (index > -1)
+            this.tasks[index].status = !this.tasks[index].status;
+        TasksStorageManager.store(this.key, this.tasks);
+    };
+    TaskManager.prototype.getTasks = function () {
+        return this.tasks;
+    };
+    return TaskManager;
+}());
+var TasksStorageManager = /** @class */ (function () {
+    function TasksStorageManager() {
+    }
+    TasksStorageManager.store = function (key, tasks) {
+        localStorage.setItem(key, JSON.stringify(tasks));
+    };
+    TasksStorageManager.load = function (key) {
+        var _a;
+        return JSON.parse((_a = localStorage.getItem(key)) !== null && _a !== void 0 ? _a : JSON.stringify([]));
+    };
+    return TasksStorageManager;
+}());
+document.getElementById("TaskForm").onsubmit =
+    handleSubmit;
+var taskManager = new TaskManager();
+function CreateTaskCard(index, _a) {
+    var task = _a.task, status = _a.status;
+    console.log(index, { task: task, status: status });
+    return "<li class=\"TaskCard scalingHover\">\n          <input type=\"checkbox\" name=\"\" ".concat(status ? "checked" : "", " id=\"\" onChange=\"toggleStatus(").concat(index, ")\" /><p class=").concat(status ? "done" : "", ">").concat(task, "</p>\n          <button class=\"deleteButton\" onClick=\"handleDelete(").concat(index, ")\"></button> \n        </li>");
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+    var task = document.getElementById("taskInput").value;
+    if (task.trim().length > 0)
+        taskManager.addTask(task);
+    document.getElementById("taskInput").value = "";
+    buildTaskList();
+}
+function handleDelete(index) {
+    taskManager.deleteTask(index);
+    buildTaskList();
+}
+function buildTaskList() {
+    var i = 0;
+    var tasks = taskManager.getTasks();
+    document.getElementById("TaskList").innerHTML =
+        tasks.length
+            ? tasks.map(function (task) { return CreateTaskCard(i++, task); }).join("\n")
+            : "<p class='mutedMessage'>no tasks</p>";
+}
+buildTaskList();
---
+++ src/script.ts
@@ -0,0 +1,80 @@
+type Task = { task: string; status: boolean };
+class TaskManager {
+  private tasks: Task[];
+  private key: string = "tasks";
+  constructor() {
+    this.tasks = TasksStorageManager.load(this.key);
+  }
+  addTask(task: string) {
+    this.tasks.push({ task, status: false });
+    TasksStorageManager.store(this.key, this.tasks);
+  }
+
+  deleteTask(index: number) {
+    if (index > -1) this.tasks.splice(index, 1);
+    TasksStorageManager.store(this.key, this.tasks);
+  }
+
+  toggleStatus(index: number) {
+    if (index > -1) this.tasks[index].status = !this.tasks[index].status;
+    TasksStorageManager.store(this.key, this.tasks);
+  }
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+}
+
+class TasksStorageManager {
+  constructor() {}
+
+  static store(key: string, tasks: Task[]) {
+    localStorage.setItem(key, JSON.stringify(tasks));
+  }
+  static load(key: string): Task[] {
+    return JSON.parse(
+      (localStorage.getItem(key) as string) ?? JSON.stringify([]),
+    ) as Task[];
+  }
+}
+
+(document.getElementById("TaskForm") as HTMLFormElement).onsubmit =
+  handleSubmit;
+let taskManager = new TaskManager();
+
+function CreateTaskCard(
+  index: number,
+  { task, status }: { task: string; status: boolean },
+) {
+  console.log(index, { task, status });
+
+  return `<li class="TaskCard scalingHover">
+          <input type="checkbox" name="" ${status ? "checked" : ""} id="" onChange="toggleStatus(${index})" /><p class=${status ? "done" : ""}>${task}</p>
+          <button class="deleteButton" onClick="handleDelete(${index})"></button> 
+        </li>`;
+}
+
+function handleSubmit(e: Event) {
+  e.preventDefault();
+  console.log("submit");
+  const task = (document.getElementById("taskInput") as HTMLInputElement).value;
+  if (task.trim().length > 0) taskManager.addTask(task);
+  (document.getElementById("taskInput") as HTMLInputElement).value = "";
+  buildTaskList();
+}
+
+function handleDelete(index: number) {
+  taskManager.deleteTask(index);
+
+  buildTaskList();
+}
+
+function buildTaskList() {
+  let i = 0;
+  const tasks = taskManager.getTasks();
+  (document.getElementById("TaskList") as HTMLUListElement).innerHTML =
+    tasks.length
+      ? tasks.map((task) => CreateTaskCard(i++, task)).join("\n")
+      : "<p class='mutedMessage'>no tasks</p>";
+}
+buildTaskList();
